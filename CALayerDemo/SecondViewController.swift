4b2923d549bca8cebcbf1b00e499b71473282add
Sergei Demianenko <siriysgrey@gmail.com>
2020-03-05 21:45:56 +0200

    ca layer

---
+++ CALayerDemo/SecondViewController.swift
@@ -0,0 +1,30 @@
+//
+//  SecondViewController.swift
+//  CALayerDemo
+//
+//  Created by Serhii Demianenko on 05.03.2020.
+//  Copyright © 2020 Serhii Demianenko. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
